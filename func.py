c152954c57d912c1b4c204c49ab43556d1205e7b
Mohammad Reza Ebadollah <63243209+ebad84@users.noreply.github.com>
2022-03-10 21:14:08 +0330

    Update func.py

--- func.py
+++ func.py
@@ -5,13 +5,19 @@ url = "http://virgool.io"
 def pyanywhere_requester(url):
     if not "http" in url:
         url = "http://" + url
-    url = f"https://api.allorigins.win/get?callback=myFunc&url={url}"
+    short_url = requests.post("http://urlshorter.pythonanywhere.com/short", data={"url":url}).json()["short_url"] # https://github.com/ebad84/url-shorter
+    # print(short_url.headers)
+    url = f"https://api.allorigins.win/get?callback=myFunc&url={short_url}"
     regex_command = r".{1,}\"contents\"\:\"(.{0,})\",\"status\""
     res = requests.get(url).text
     out = re.findall(regex_command, res)
+    # print(out[0].strip())
     try:
-        return str(out[0])
+        out = out[0].replace(r"\r", "").replace(r"\n","").replace('\\"', '"').replace(r"\t", "")
+        # print(out)
+        return str(out)
     except:
         return "something is wrong i can feel it..."
+
         
 print(pyanywhere_requester(url))
